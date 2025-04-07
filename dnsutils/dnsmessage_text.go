c5da740833fd457b2c47f2da751da1022f6566f0
dmachard <5562930+dmachard@users.noreply.github.com>
2025-04-07 18:48:54 +0200

    feat: new text directive to get answer ip

--- dnsutils/dnsmessage_text.go
+++ dnsutils/dnsmessage_text.go
@@ -540,6 +540,43 @@ func (dm *DNSMessage) ToTextLine(format []string, fieldDelimiter string, fieldBo
 				s.WriteByte('-')
 			}
 
+		case directive == "answer-a":
+			ans_found := false
+			for _, answer := range an {
+				if answer.Rdatatype == "A" {
+					s.WriteString(answer.Rdata)
+					ans_found = true
+					break
+				}
+			}
+			if !ans_found {
+				s.WriteByte('-')
+			}
+		case directive == "answer-aaaa":
+			ans_found := false
+			for _, answer := range an {
+				if answer.Rdatatype == "AAAA" {
+					s.WriteString(answer.Rdata)
+					ans_found = true
+					break
+				}
+			}
+			if !ans_found {
+				s.WriteByte('-')
+			}
+		case directive == "answer-ip":
+			ans_found := false
+			for _, answer := range an {
+				if answer.Rdatatype == "A" || answer.Rdatatype == "AAAA" {
+					s.WriteString(answer.Rdata)
+					ans_found = true
+					break
+				}
+			}
+			if !ans_found {
+				s.WriteByte('-')
+			}
+
 		case directive == "questionscount" || directive == "qdcount":
 			s.WriteString(strconv.Itoa(dm.DNS.QdCount))
 		case directive == "answercount" || directive == "ancount":
