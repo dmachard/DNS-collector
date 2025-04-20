367e3780c2f63cb90d8b1f920212972636e2d713
dmachard <5562930+dmachard@users.noreply.github.com>
2025-04-20 10:05:16 +0200

    add log message

--- tests/dnsquery_dnstapunix.py
+++ tests/dnsquery_dnstapunix.py
@@ -50,6 +50,7 @@ class TestDnstap(unittest.TestCase):
             # run collector
             is_ready = asyncio.Future()
             is_clientresponse = asyncio.Future()
+            print("Starting collector with current user: ", COLLECTOR_USER)
             args = ( "sudo", "-u", COLLECTOR_USER, "-s", "./dnscollector", "-config", "./tests/testsdata/config_stdout_dnstapunix.yml",)
             transport_collector, protocol_collector =  await self.loop.subprocess_exec(lambda: ProcessProtocol(is_ready, is_clientresponse),
                                                                                        *args, stdout=asyncio.subprocess.PIPE)
