39581c1995d7600307e5dca82ff90c451e37c2a9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-18 15:10:33 -0400

    spelling: whenever
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- workers/prometheus.go
+++ workers/prometheus.go
@@ -103,7 +103,7 @@ type PromCounterCatalogueContainer struct {
 	// labelNames - is a list of label *names* for PromCounterCatalogueContainer's in stats
 	// map to use to get proper selectors.
 	// The topmost instance of PromCounterCatalogueContainer has the full list of all names to
-	// consider (the one provided by the config). Whenver it needs to create a new item in
+	// consider (the one provided by the config). Whenever it needs to create a new item in
 	// it's stats map, it supplies labelNames[1:] to the constructor for the lower level
 	// container to get the selector for the next level
 	labelNames []string // This is list of label names for nested containers
