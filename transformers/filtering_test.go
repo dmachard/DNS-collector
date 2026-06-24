25f49fd1ddefb335eda3d5c55f9d016ab1a8fc14
dmachard <5562930+dmachard@users.noreply.github.com>
2026-06-24 15:22:34 +0200

    perf: optimize domain regex filtering by using combined patterns and add benchmark tests

--- transformers/filtering_test.go
+++ transformers/filtering_test.go
@@ -1,6 +1,8 @@
 package transformers
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/dmachard/go-dnscollector/v2/dnsutils"
@@ -470,5 +472,35 @@ func TestFilteringDownsampleFilter(t *testing.T) {
 
 	if kept != 10/N {
 		t.Errorf("expected %d messages to be kept, got %d", 10/N, kept)
+	}
+}
+
+func BenchmarkFiltering_DropDomainRegex(b *testing.B) {
+	// Create a temporary file with 100 regex patterns
+	tmpFile, err := os.CreateTemp("", "filtering_regex_*.txt")
+	if err != nil {
+		b.Fatalf("failed to create temp file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	for i := 0; i < 100; i++ {
+		_, _ = fmt.Fprintf(tmpFile, "domain%d\\.com$\n", i)
+	}
+	tmpFile.Close()
+
+	config := pkgconfig.GetFakeConfigTransformers()
+	config.Filtering.Enable = true
+	config.Filtering.DropDomainFile = tmpFile.Name()
+
+	outChans := []chan dnsutils.DNSMessage{}
+	filtering := NewFilteringTransform(config, logger.New(false), "test", 0, outChans)
+	_, _ = filtering.GetTransforms()
+
+	dm := dnsutils.GetFakeDNSMessage()
+	dm.DNS.Qname = "notmatchingdomain.com"
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = filtering.dropDomainRegexFilter(&dm)
 	}
 }
