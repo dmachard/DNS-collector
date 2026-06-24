e767bbcf419e8db7501795457df66222af9e889c
dmachard <5562930+dmachard@users.noreply.github.com>
2026-06-24 11:02:39 +0200

    perf(reordering): optimize reordering transform with double buffering, pre-calculated timestamps, and added benchmarks

--- transformers/reordering_test.go
+++ transformers/reordering_test.go
@@ -3,6 +3,7 @@ package transformers
 import (
 	"sort"
 	"testing"
+	"time"
 
 	"github.com/dmachard/go-dnscollector/v2/dnsutils"
 	"github.com/dmachard/go-dnscollector/v2/pkgconfig"
@@ -66,5 +67,42 @@ func TestReorderingTransform_SortByTimestamp(t *testing.T) {
 
 	if !sort.StringsAreSorted(timestamps) {
 		t.Errorf("Timestamps are not sorted: %v", timestamps)
+	}
+}
+
+func BenchmarkReordering_ReorderAndFlush(b *testing.B) {
+	config := pkgconfig.GetFakeConfigTransformers()
+	config.Reordering.Enable = true
+	config.Reordering.MaxBufferSize = 1000
+
+	log := logger.New(false)
+	outChans := []chan dnsutils.DNSMessage{
+		make(chan dnsutils.DNSMessage, 2000),
+	}
+
+	reorder := NewReorderingTransform(config, log, "test", 0, outChans)
+
+	// Create 1000 fake messages with different timestamps
+	messages := make([]dnsutils.DNSMessage, 1000)
+	for i := 0; i < 1000; i++ {
+		messages[i] = dnsutils.GetFakeDNSMessage()
+		ts := time.Now().Add(time.Duration(i%2-i%3+i%5) * time.Millisecond)
+		messages[i].DNSTap.Timestamp = ts.UnixNano()
+		messages[i].DNSTap.TimestampRFC3339 = ts.Format(time.RFC3339Nano)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// Fill buffer
+		for j := 0; j < 1000; j++ {
+			reorder.buffer = append(reorder.buffer, messages[j])
+		}
+		// Flush buffer
+		reorder.flushBuffer()
+
+		// Drain output channel
+		for len(outChans[0]) > 0 {
+			<-outChans[0]
+		}
 	}
 }
