d8a7cbc0ad93e6c7ce767efa918e9d9561628a21
dmachard <5562930+dmachard@users.noreply.github.com>
2026-08-23 20:16:18 +0200

    perf(transformers): optimize reordering with zero-alloc slices.SortFunc and batched flush

--- transformers/reordering_bench_test.go
+++ transformers/reordering_bench_test.go
@@ -1,6 +1,10 @@
 package transformers
 
 import (
+	"cmp"
+	"math/rand"
+	"slices"
+	"sort"
 	"testing"
 	"time"
 
@@ -9,41 +13,115 @@ import (
 	"github.com/dmachard/go-logger"
 )
 
-func BenchmarkReordering_ReorderAndFlush(b *testing.B) {
-	config := pkgconfig.GetFakeConfigTransformers()
-	config.Reordering.Enable = true
-	config.Reordering.MaxBufferSize = 1000
-
-	log := logger.New(false)
-	outChans := []chan *dnsutils.DNSMessageBatch{
-		make(chan *dnsutils.DNSMessageBatch, 2000),
+func generateMessages(n int, jitter int64) []*dnsutils.DNSMessage {
+	baseTime := time.Now().UnixNano()
+	msgs := make([]*dnsutils.DNSMessage, n)
+	r := rand.New(rand.NewSource(42))
+	for i := 0; i < n; i++ {
+		dm := dnsutils.GetFakeDNSMessage()
+		var offset int64
+		if jitter > 0 {
+			offset = int64(i)*1_000_000 + r.Int63n(jitter) - jitter/2
+		} else {
+			offset = r.Int63n(1_000_000_000)
+		}
+		dm.DNSTap.Timestamp = baseTime + offset
+		msgs[i] = &dm
 	}
+	return msgs
+}
 
-	reorder := NewReorderingTransform(config, log, "test", 0, outChans)
+// -------------------------------------------------------------
+// Pure Algorithm Microbenchmarks (sort.SliceStable vs slices.SortFunc)
+// -------------------------------------------------------------
 
-	// Create 1000 fake messages with different timestamps
-	messages := make([]*dnsutils.DNSMessage, 1000)
-	for i := 0; i < 1000; i++ {
-		msg := dnsutils.GetFakeDNSMessage()
-		ts := time.Now().Add(time.Duration(i%2-i%3+i%5) * time.Millisecond)
-		msg.DNSTap.Timestamp = ts.UnixNano()
-		msg.DNSTap.TimestampRFC3339 = ts.Format(time.RFC3339Nano)
-		messages[i] = &msg
-	}
+func Benchmark_Sort_SliceStable_NearlySorted_1000(b *testing.B) {
+	template := generateMessages(1000, 50_000_000)
+	buf := make([]*dnsutils.DNSMessage, 1000)
 
 	b.ReportAllocs()
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		// Fill buffer
-		for j := 0; j < 1000; j++ {
-			reorder.buffer = append(reorder.buffer, messages[j])
-		}
-		// Flush buffer
-		reorder.flushBuffer()
 
-		// Drain output channel
+	for i := 0; i < b.N; i++ {
+		copy(buf, template)
+		sort.SliceStable(buf, func(x, y int) bool {
+			return buf[x].DNSTap.Timestamp < buf[y].DNSTap.Timestamp
+		})
+	}
+}
+
+func Benchmark_Sort_SlicesSortFunc_NearlySorted_1000(b *testing.B) {
+	template := generateMessages(1000, 50_000_000)
+	buf := make([]*dnsutils.DNSMessage, 1000)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		copy(buf, template)
+		slices.SortFunc(buf, func(a, b *dnsutils.DNSMessage) int {
+			return cmp.Compare(a.DNSTap.Timestamp, b.DNSTap.Timestamp)
+		})
+	}
+}
+
+func Benchmark_Sort_SliceStable_Random_1000(b *testing.B) {
+	template := generateMessages(1000, 0)
+	buf := make([]*dnsutils.DNSMessage, 1000)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		copy(buf, template)
+		sort.SliceStable(buf, func(x, y int) bool {
+			return buf[x].DNSTap.Timestamp < buf[y].DNSTap.Timestamp
+		})
+	}
+}
+
+func Benchmark_Sort_SlicesSortFunc_Random_1000(b *testing.B) {
+	template := generateMessages(1000, 0)
+	buf := make([]*dnsutils.DNSMessage, 1000)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		copy(buf, template)
+		slices.SortFunc(buf, func(a, b *dnsutils.DNSMessage) int {
+			return cmp.Compare(a.DNSTap.Timestamp, b.DNSTap.Timestamp)
+		})
+	}
+}
+
+// -------------------------------------------------------------
+// Transformer FlushBuffer End-to-End Benchmarks
+// -------------------------------------------------------------
+
+func Benchmark_Reordering_FlushBuffer_1000(b *testing.B) {
+	config := pkgconfig.GetFakeConfigTransformers()
+	config.Reordering.Enable = true
+	config.Reordering.MaxBufferSize = 1000
+	log := logger.New(false)
+	outChans := []chan *dnsutils.DNSMessageBatch{make(chan *dnsutils.DNSMessageBatch, 2000)}
+	reorder := NewReorderingTransform(config, log, "bench", 0, outChans)
+
+	template := generateMessages(1000, 50_000_000) // nearly sorted
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		reorder.mutex.Lock()
+		reorder.buffer = append(reorder.buffer[:0], template...)
+		reorder.mutex.Unlock()
+
+		reorder.flushBuffer()
+		// drain channel
 		for len(outChans[0]) > 0 {
-			<-outChans[0]
+			batch := <-outChans[0]
+			batch.Release()
 		}
 	}
 }
