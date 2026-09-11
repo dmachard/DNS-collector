ca27ab1bf4b128f8e3730d515ebf5f0ced0c52e9
James Ko <james.llewellyn.ko@gmail.com>
2026-09-11 10:13:33 -0400

    Close gzip writer when bulk compression fails (#1370)
    * Close gzip writer when bulk compression fails
    
    * Handle gzip writer close on write failure
    
    ---------
    
    Co-authored-by: James Ko <jamesqko@gmail.com>
    Co-authored-by: James Ko <james@v2202608401667503709.supersrv.de>

--- workers/elasticsearch.go
+++ workers/elasticsearch.go
@@ -281,6 +281,7 @@ func (w *ElasticSearchClient) sendCompressedBulk(bulk []byte) error {
 	gzipWriter := gzip.NewWriter(&compressedBulk)
 	_, err := gzipWriter.Write(bulk)
 	if err != nil {
+		_ = gzipWriter.Close()
 		return err
 	}
 	err = gzipWriter.Close()
